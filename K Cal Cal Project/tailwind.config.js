7632fc4e5300fc16f188f5b5ec42dec8405de98a
FILMMY <88100445+FILMMY@users.noreply.github.com>
2023-10-05 05:14:28 -0700

    Add files via upload

---
+++ K Cal Cal Project/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'greenHover' : '#3d847d',
+      'pink' : '#f272b5',
+      'pinkFocus' : '#eb258d',
+      'blueFocus':'#003dc3',
+      'blue' : '#4877e1',
+      'red' : '#f80000',
+      'redFocus': '#b00000',
+      'white':"#FFFFFF",
+    },
+    fontFamily: {
+      rbtM: ['Roboto-Medium', 'sans-serif'],
+      rbtB: ['Roboto-Bold', 'sans-serif']
+    }
+  },
+  plugins: [require("daisyui")],
+}
